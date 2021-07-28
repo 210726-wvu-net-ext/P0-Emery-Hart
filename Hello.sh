606a9d219f8fdb55313c6c623543d70512aebf7d
dutran <lovesandlife@gmail.com>
2021-07-28 13:56:38 -0400

    add new fild hello to Emery

--- Hello.sh
+++ Hello.sh
@@ -1,7 +1,3 @@
-#! /usr/bin/bash
-
-#Prompts user for three favorite movies and then prints them 
-
-echo "Enter your three favorite movies (separated by a single space, uses a - for in-title spaces): "
-read -a movies 
-echo "The movies are: ${movies[0]}, ${movies[1]}, ${movies[2]}"
+echo "Enter your top 3 favorite movies: "
+read movie1 movie2 movie3
+echo "Your top 3 favorite movies are: $movie1 $movie2 $movie3"
