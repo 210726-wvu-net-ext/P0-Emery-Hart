671de0d45bd93ad64da21b84c1baf65c0f393812
Ehart <Ehart3@masonlive.gmu.edu>
2021-08-17 17:31:56 -0400

    Hidden connection string

--- Resturant Reviewer/App/Program.cs
+++ Resturant Reviewer/App/Program.cs
@@ -1,5 +1,25 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using BL;
+using DL;
+using DL.Entities;
 
+
+// Connection for our DB
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string connectionStr = configuration.GetConnectionString("projDB");
+
+DbContextOptions<PojectzeroContext> option = new DbContextOptions<PojectzeroContext>()
+    .UseSqlServer(connectionStr)
+    .Options;
+
+var context = new PojectzeroContext(options);    
 namespace App
 {
     class Program
--- Resturant Reviewer/DL/Entities/PojectzeroContext.cs
+++ Resturant Reviewer/DL/Entities/PojectzeroContext.cs
@@ -22,15 +22,6 @@ namespace DL.Entities
         public virtual DbSet<Style> Styles { get; set; }
         public virtual DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:projectzero.database.windows.net,1433;Initial Catalog=Poject zero;Persist Security Info=False;User ID=projAdmin;Password=REVpass2021$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
